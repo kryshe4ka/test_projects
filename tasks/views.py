b34969c87dd7b58e468c7bd6643d760ca2c3ca63
Lisa Kryshkovskaya <lisakryshkovskaya@MacBook-Air-Lisa.local>
2021-01-05 16:44:41 +0300

    Create an index function in views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+tasks = ["foo", "bar", "zaz"]
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
