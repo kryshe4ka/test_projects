92ebe8785e094e7fa35ad314df5bec403acdac7c
Lisa Kryshkovskaya <lisakryshkovskaya@MacBook-Air-Lisa.local>
2021-01-05 17:10:40 +0300

    Add form, tasks list, links to navigate

--- tasks/views.py
+++ tasks/views.py
@@ -6,3 +6,6 @@ def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
+# Add a new task:
+def add(request):
+    return render(request, "tasks/add.html")
